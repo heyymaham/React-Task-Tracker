089ffdaf3dd59d26889025c80e12eb3c5817d719
heyymaham <heyymaham@gmail.com>
2024-01-30 10:58:47 +0500

    Initial commit

---
+++ src/Components/Footer.js
@@ -0,0 +1,14 @@
+/** @format */
+
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Copyright &copy; 2024</p>
+      <Link to="/about">About</Link>
+    </footer>
+  );
+};
+
+export default Footer;
